1c5949d305bb83817ce724756fd2d475acc147a0
JCamyre <jwcamry03@gmail.com>
2022-05-17 21:21:15 -0700

    Basic structure

---
+++ api/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const { application } = require("express");
+
+const app = express();
+// This so we can send json responses?
+app.use(express.json());
+// something about accessing the api endpoints
+app.use(express.urlencoded({ extended: true }));
+
+// get request to '/'
+app.get("/", (req, res) => {
+  res.send("Hi there");
+});
+
+// Send web scraped info from website designated by url
+app.get("/webscrape", (req, res) => {
+  res.send(req);
+});
+// could also do this like /webscrape/:url
